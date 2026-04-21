3ba89b11d75640e4afa582a99428e6f8507d62fa
unknown <reccberry@yandex.ru>
2026-04-22 00:10:13 +0300

    updated

--- lab4/example-express/src/index.js
+++ lab4/example-express/src/index.js
@@ -1,41 +1,35 @@
 const express = require('express');
 const path = require('path');
-const equipmentRouter = require('./routes/equipment');
-const equipmentService = require('./services/equipmentService');
+const stocksRouter = require('./routes/equipment');
+const stocksService = require('./services/equipmentService');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-// Определяем путь к файлу данных
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 const DATA_FILE_PATH = path.join(__dirname, 'data/equipment.json');
 
-// Инициализируем сервис с путем к файлу данных
-equipmentService.init(DATA_FILE_PATH);
+stocksService.init(DATA_FILE_PATH);
 
-// 1. Встроенный middleware для парсинга JSON
 app.use(express.json());
 
-// 2. Логирующий middleware
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next(); // Обязательно вызываем next(), иначе запрос зависнет
+    next();
 });
 
-// 3. Подключение маршрутов
-app.use('/equipment', equipmentRouter);
+app.use('/equipment', stocksRouter);
 
-// 4. Глобальная обработка 404
 app.use((req, res) => {
     res.status(404).json({ error: 'Маршрут не найден' });
 });
 
-// error handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ error: 'Внутренняя ошибка сервера' });
 });
 
-// 5. Запуск сервера
 app.listen(PORT, () => {
     console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
 });
